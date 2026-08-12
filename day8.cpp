a4bd4fd5a094dad3d595831996eecb6e2f1be2b7
ansh0724 <anshraj0724@gmail.com>
2026-08-12 18:39:35 +0530

    commit

---
+++ day8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// int main(){
+//     int arr[] = {3,4,5,6,7,0,1,2};
+//     int target =0;
+//     int sz=8;
+//     int st=0,end=sz-1;
+//     int idx=0;
+//     while(st <= end){
+//         int mid = st + (end-st)/2;
+//         if(target == arr[mid]){
+//             idx= mid+1;
+//         }
+//         if(arr[st] <= arr[mid]){
+//             if(arr[st] <= target && target <= arr[mid]){
+//                 end = mid-1;
+//             } else {
+//                 st = mid+1;   
+//             }
+//         } else if(arr[end] >= target && target >= arr[mid]){
+//             st = mid+1;
+//         } else {
+//             end = mid-1;      
+//         }
+        
+//     }
+//     cout << idx<<endl;
+//     return 0;
+// }
+
+int main(){
+                      
+}
