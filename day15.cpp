e16e15700f6dc25e28769e4b2295d27d32dee380
ansh0724 <anshraj0724@gmail.com>
2026-08-27 18:41:41 +0530

    commit

---
+++ day15.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    // int matrix[4][3]= {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    // int rows=4;
+    // int col = 3;
+    // int maxSum= INT_MIN;
+    // for(int i=0;i < rows;i++){
+    //     int count =0;
+    //     for(int j=0;j<col;j++){
+    //         count += matrix[i][j];
+            
+    //     }
+    //     if(count > maxSum){
+    //         maxSum= count;
+    //     }
+    // }                           
+    // cout << maxSum;
+    // // cout << martix[2][1];        
+    // for(int i=0;i < rows;i++){
+    //     for(int j =0;j<col;j++){
+    //         cin >> martix[i][j];
+    //     }
+    //     // cout<<endl;
+    // }                              
+    // for(int i=0;i < rows;i++){
+    //     for(int j =0;j<col;j++){
+    //         cout << martix[i][j];
+    //     }
+    //     // cout<<endl;
+    // }   
+
+    // vector<vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+
+    //search in a 2d matrix
+
+    // vector<vector<int>> mat = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    // int m = mat.size(), n = mat[0].size();
+    // int target = 34;
+    // int stRow =0, endRow = m-1;
+    // while(stRow <= endRow){
+    //     int midRow = stRow + (endRow - stRow)/2;
+    //     if(target >= mat[midRow][0] && target <= mat[midRow][n-1]){
+
+    //     }else if( target >= mat[midRow][n-1]){
+    //         stRow = midRow +1;
+    //     }else{
+    //         endRow= midRow-1
+    //     }
+    // }
+    // vector<vector<int>> mat ={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    // int rows = mat.size();
+    // int cols=mat[0].size();
+    // // cout << rows;
+    // // cout << cols;
+    // int target = 35;
+    // int st=0,end=rows*cols-1;
+    // while(st <= end){
+    //     int mid = st +(end-st)/2;
+    //     int row = mid/cols;
+    //     int col = mid%cols;
+    //     if(target == mat[row][col]){
+    //         cout << row+1<<" "<< col+1<<endl;
+    //         break;
+    //     }else if(target > mat[row][col]){
+    //         st = mid+1;
+    //     }else{
+    //         end = mid-1;
+    //     }
+        
+
+    // }
+
+      
+    
+}
