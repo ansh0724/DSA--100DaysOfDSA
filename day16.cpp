ac1e3a9bac4e83d03297aa5601342669f48a2b83
ansh0724 <anshraj0724@gmail.com>
2026-08-31 18:44:10 +0530

    commit

---
+++ day16.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+int main(){
+    int arr[] = {5,2,11,7,15};
+    // sort(arr, arr + 5);
+    // int st = 0;
+    int target = 9;
+    // int end = 4;
+    // while(st <= end){
+    //     if(arr[st]+arr[end] == target){
+    //         cout << "(" << st + 1 << ", " << end + 1 << ")";
+    //         break;
+    //     } else if(arr[st] + arr[end] < target){
+    //         st++;
+    //     } else {
+    //         end--;
+    //     }
+    // }
+    // unordered_map<int,int> m;
+    // vector<int> ans;
+    // for(int i =0; i< 5;i++){
+    //     int first=arr[i];
+    //     int sec = target-first;
+    //     if(m.find(sec) != m.end()){
+
+    //         ans.push_back(m[sec]);
+    //         ans.push_back(i);
+    //         break;
+    //     }
+    //     m[first] = i;
+    // }
+    // for(int i=0;i<ans.size();i++){
+    //     cout << ans[i]<<" ";
+    // }
+
+    
+}
